6125bbb03f37e8c19f76c20e7d847670c497619f
alekseykrazhev <kr69@mail.ru>
2021-02-19 11:07:46 +0300

    Class task completed

---
+++ class_task/class_task/class_task.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main() {
+    std::ifstream in("C:\\Users\\Alex\\Desktop\\Project1\\class_task\\in.txt");
+    if (!in.is_open()) {
+        std::cerr << "ERROR OPENING FILE";
+        return 1;
+    }
+    int a, find;
+    bool is_found = false;
+    std::vector<int> vec;
+    while (in >> a) {
+        vec.push_back(a);
+    }
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Enter a number ";
+        std::cin >> find;
+        for (int j = 0; j < vec.size(); ++j) {
+            if (vec[j] == find) {
+                std::cout << "Number " << find << " is in array :)\n";
+                is_found = true;
+                continue;
+            }
+        }
+        if (!is_found) {
+            std::cout << "Number " << find << " isn't in array :(\n";
+        }
+        is_found = false;
+    }
+    return 0;
+}
