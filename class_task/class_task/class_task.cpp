f969d2293d4afba28990ce710b535b4e739557b8
alekseykrazhev <kr69@mail.ru>
2021-02-19 11:33:00 +0300

    magic number is now constant

--- class_task/class_task/class_task.cpp
+++ class_task/class_task/class_task.cpp
@@ -9,23 +9,24 @@ int main() {
         return 1;
     }
     int a, find;
+    const int amount = 5;
     bool is_found = false;
     std::vector<int> vec;
     while (in >> a) {
         vec.push_back(a);
     }
-    for (int i = 0; i < 5; ++i) {
-        std::cout << "Enter a number ";
+    for (int i = 0; i < amount; ++i) {
+        std::cout << "Enter a number : ";
         std::cin >> find;
         for (int j = 0; j < vec.size(); ++j) {
             if (vec[j] == find) {
-                std::cout << "Number " << find << " is in array :)\n";
+                std::cout << "Number " << find << " is in array:)\n";
                 is_found = true;
                 continue;
             }
         }
         if (!is_found) {
-            std::cout << "Number " << find << " isn't in array :(\n";
+            std::cout << "Number " << find << " isn't in array:(\n";
         }
         is_found = false;
     }
