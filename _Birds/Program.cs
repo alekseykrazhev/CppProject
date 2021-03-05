774e7ba71e3903ef41fdd47c8b14959354f62019
alekseykrazhev <kr69@mail.ru>
2021-03-05 10:04:25 +0300

    fixed

---
+++ _Birds/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _Birds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // enter name, weight, sex, age
+            Bird check = new Kiwi("Kiwiiii", 0.25, Bird.Sex.Male, 1);
+            Bird parrot = new Parrot("Artur", 0.12, Bird.Sex.Female, 0.7);
+            Bird duck = new Duck("Donald", 0.45, Bird.Sex.Male, 2);
+            if (!duck.CanFly())
+            {
+                Console.WriteLine("Duck can't fly :(");
+            }
+            if (!check.CanFloat())
+            {
+                Console.WriteLine("Apparently, kiwi can't float");
+            }
+
+            check.Name = "Not Kiwiiii";
+            Console.WriteLine(check.Name);
+            Console.WriteLine(check.Gender);
+            parrot.Weight = 0.22;
+            Console.WriteLine(parrot.Weight);
+            duck.Age = 3;
+            Console.WriteLine(duck.Age);
+        }
+    }
+}
---
+++ _Birds/bird.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _Birds
+{
+    public interface IFloating
+    {
+        bool CanFly();
+    }
+    public interface IFlying
+    {
+        bool CanFloat();
+    }
+    abstract class Bird
+    {
+        protected Bird (string name, double weight, Sex sex, double age)
+        {
+            Name = name;
+            Weight = weight;
+            Gender = sex;
+            Age = age;
+            Check();
+        }
+
+        private void Check()
+        {
+            if (Weight <= 0)
+            {
+                throw new Exception("Weight is way too small!");
+            }
+
+            if (Name == null)
+            {
+                throw new Exception("Give it a name!");
+            }
+        }
+
+        public bool CanFly()
+        {
+            return this is IFlying;
+        }
+
+        public bool CanFloat()
+        {
+            return this is IFloating;
+        }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Age { get; set; }
+        public enum Sex
+        {
+            Male,
+            Female
+        }
+        public Sex Gender { get; }
+        
+    }
+    class Kiwi : Bird
+    {
+        public Kiwi(string name, double weight, Sex sex, double age) : base (name, weight, sex, age)
+        { }
+    }
+    class Duck : Bird, IFloating, IFlying
+    {
+        public Duck(string name, double weight, Sex sex, double age) : base(name, weight, sex, age)
+        { }
+    }
+    class Penguin : Bird, IFloating
+    {
+        public Penguin(string name, double weight, Sex sex, double age) : base(name, weight, sex, age)
+        { }
+        
+    }
+    class Parrot : Bird, IFlying
+    {
+        public Parrot(string name, double weight, Sex sex, double age) : base(name, weight, sex, age)
+        { }
+        
+    }
+}
